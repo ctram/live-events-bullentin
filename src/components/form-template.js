8be3d77d5cb74f2e0f919c89cb95a4b18378ee88
Chris Tram <chris.tram@gmail.com>
2018-04-08 18:48:25 -0700

    cancel editmode when template is updated for the form template view.

--- src/components/form-template.js
+++ src/components/form-template.js
@@ -24,7 +24,7 @@ export class FormTemplate extends React.Component {
 
   componentWillReceiveProps(nextProps) {
     const { templateSelector, templateName, templateUrl } = nextProps;
-    this.setState({ templateSelector, templateName, templateUrl });
+    this.setState({ templateSelector, templateName, templateUrl, editMode: false });
   }
 
   addTemplate(e) {
