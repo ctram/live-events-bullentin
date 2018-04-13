6cf2b1af6cfd572a7b095057dee340a876f29489
Chris Tram <chris.tram@gmail.com>
2018-04-13 15:21:01 -0700

    adds package json lock file

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
-const webpack = require('webpack');
 const path = require('path');
 
-const sharedOptions = {
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-    port: 3000
+const sharedConfig = {
+  watch: true
+};
+
+const serverConfig = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
   },
-  plugins: [new webpack.NamedModulesPlugin(), new webpack.HotModuleReplacementPlugin()],
+  target: 'node'
+};
+Object.assign(serverConfig, sharedConfig);
+
+const clientConfig = {
+  mode: 'development',
+  entry: './src/index.js',
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              // need this to use es6 class extends feature
-              ['babel-preset-env', { exclude: ['transform-es2015-classes'] }],
-              ['babel-preset-react'],
-              ['babel-preset-stage-1']
-            ]
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        loader: 'babel-loader',
+        options: { presets: ['react', 'env'] }
       },
       {
         test: /\.scss$/,
@@ -42,35 +44,14 @@ const sharedOptions = {
             loader: 'sass-loader' // compiles Sass to CSS
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
-  },
-  watch: false,
-  devtool: 'source-map'
-};
-
-const serverConfig = {
-  entry: {
-    app: './src/app.js'
-  },
-  target: 'node',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].node.js'
   }
 };
-Object.assign(serverConfig, sharedOptions);
-
-const clientConfig = {
-  entry: {
-    index: './src/index.js'
-  },
-  target: 'web',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
-  }
-};
-Object.assign(clientConfig, sharedOptions);
+Object.assign(clientConfig, sharedConfig);
 
 module.exports = [serverConfig, clientConfig];
