4fc9ea08745d979c604b3a810bc2596d1f93283d
Chris Tram <chris.tram@gmail.com>
2018-04-23 18:45:48 -0700

    manually import models into sequelize

--- models/index.js
+++ models/index.js
@@ -1,37 +1,13 @@
 'use strict';
-
-import fs from 'fs';
-import path from 'path';
 import Sequelize from 'sequelize';
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require($dirname + '/config/config.json')[env];
-const db = {};
-let sequelize;
-
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_constiable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync($dirname + '/models')
-  .filter(file => {
-    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join($dirname + '/models', file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+import sequelize from './sequelize';
+import User from './user';
+import Website from './website';
+const db = { User, Website };
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+console.log('user' , db.User)
+
+export default db;
