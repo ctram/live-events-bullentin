db872ea76b2225c59ea45235272ca45c72ccb626
Chris Tram <chris.tram@gmail.com>
2018-05-05 00:25:33 -0700

    adds method to fetch events for website model.

--- backbone/models/website.js
+++ backbone/models/website.js
@@ -1,3 +1,26 @@
 import Backbone from 'backbone';
+import appConfig from '../../app-config';
+import requestParams from '../../helpers/request-params';
 
-export default class Website extends Backbone.Model {}
+export default class Website extends Backbone.Model {
+  urlRoot() {
+    return `${appConfig.serverUrl}/api/websites`;
+  }
+
+  parse(res) {
+    const { id, selector, url, name } = res.website;
+    return { id, selector, url, name };
+  }
+
+  fetchEvents() {
+    const req = new Request(
+      appConfig.serverUrl + `/api/websites/${this.id}/events`,
+      Object.assign(requestParams, { method: 'GET', body: null })
+    );
+
+    return fetch(req).then(res => {
+      this.set('events', res.events);
+      return res.events;
+    });
+  }
+}
