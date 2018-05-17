9c697ab6c29656e896a272888ced0a6d8a2a088d
Chris Tram <chris.tram@gmail.com>
2018-05-17 16:06:18 -0700

    simplify logical for passport authentication

--- app.js
+++ app.js
@@ -25,11 +25,8 @@ passport.use(
   new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
     User.findOne({ where: { email } })
       .then(user => {
-        if (!user) {
-          return done(null, false, { message: 'Incorrect email.' });
-        }
-        if (user.password !== password) {
-          return done(null, false, { message: 'Incorrect password.' });
+        if (!user || user.password !== password) {
+          return done(null, false, { message: 'Incorrect email or password' });
         }
         return done(null, user);
       })
