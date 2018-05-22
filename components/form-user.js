33ca12e984da96a5e9dfbf95258e8d014f95b518
Chris Tram <chris.tram@gmail.com>
2018-05-22 16:25:37 -0700

    working on letting user choose role on registration

--- components/form-user.js
+++ components/form-user.js
@@ -6,42 +6,54 @@ class FormUser extends React.Component {
     this.pathname = props.location.pathname.substring(1);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.btnSubmitDisabled = this.btnSubmitDisabled.bind(this);
-    this.state = { email: '', password: '' };
+    this.toggleRole = this.toggleRole.bind(this);
+    this.state = { username: '', password: '', role: 'standard' };
   }
 
   componentWillReceiveProps(nextProps) {
     this.pathname = nextProps.location.pathname.substring(1);
   }
 
+  toggleRole(role) {
+    this.setState({ role });
+  }
+
   handleSubmit(e) {
     e.preventDefault();
     const { createUser, loginUser } = this.props;
-    const { email, password } = this.state;
-    const data = { email, password };
-    this.pathname === 'register' ? createUser(data) : loginUser(data);
+    const { username, password, role } = this.state;
+    this.pathname === 'register'
+      ? createUser({ username, password, role })
+      : loginUser({ username, password });
   }
 
   btnSubmitDisabled() {
-    const { password, email } = this.state;
-    return !password || !email;
+    const { password, username } = this.state;
+    return !password || !username;
   }
 
   render() {
     const labelSubmitBtn = this.pathname === 'register' ? 'Register' : 'Login';
+    const btnRoleBaseClass = 'btn btn-secondary';
+    const { role: activeRole } = this.state;
+    const btnRoleStandardClass =
+      activeRole === 'standard' ? btnRoleBaseClass + ' active' : btnRoleBaseClass;
+    const btnRoleAdminClass =
+      activeRole === 'admin' ? btnRoleBaseClass + ' active' : btnRoleBaseClass;
+
     return (
       <div className="row justify-content-center">
         <div className="col-2">
           <form>
             <div className="form-group">
-              <label htmlFor="email">Email address</label>
+              <label htmlFor="username">Username</label>
               <input
-                type="email"
+                type="username"
                 className="form-control"
-                id="email"
-                ref="email"
-                aria-describedby="emailHelp"
-                placeholder="Enter email"
-                onChange={e => this.setState({ email: e.target.value })}
+                id="username"
+                ref="username"
+                placeholder="Enter username"
+                onChange={e => this.setState({ username: e.target.value })}
               />
             </div>
             <div className="form-group">
@@ -55,6 +67,44 @@ class FormUser extends React.Component {
                 onChange={e => this.setState({ password: e.target.value })}
               />
             </div>
+            <div className="form-group">
+              <label for="radio-role">
+                <radiogroup id="radio-role" className="btn-group">
+                  <radio className={`${btnRoleAdminClass}`} selected={activeRole === 'admin'} >
+                    One
+                  </radio>
+                  <radio className={`${btnRoleStandardClass}`} selected={activeRole === 'standard'}>
+                    One
+                  </radio>
+                </radiogroup>
+              </label>
+            </div>
+
+            {this.pathname === 'register' && (
+              <div className="form-group">
+                <label>User Role</label>
+                <div className="btn-group">
+                  <button
+                    className={`${btnRoleStandardClass}`}
+                    onClick={e => {
+                      e.preventDefault();
+                      this.toggleRole('standard');
+                    }}
+                  >
+                    Standard
+                  </button>
+                  <button
+                    className={`${btnRoleAdminClass}`}
+                    onClick={e => {
+                      e.preventDefault();
+                      this.toggleRole('admin');
+                    }}
+                  >
+                    Admin
+                  </button>
+                </div>
+              </div>
+            )}
             <button
               type="submit"
               className="btn btn-primary"
