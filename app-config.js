a3b0d615d08ab187edaea909c29c7689ba735f46
Chris Tram <chris.tram@gmail.com>
2018-06-02 15:38:12 -0700

    refactor authenticate flag in app config.

--- app-config.js
+++ app-config.js
@@ -1,6 +1,7 @@
 let port;
 let host;
 let serverUrl;
+let authenticate;
 
 console.log('environment', process.env.ENVIRONMENT);
 console.log('process env', process.env);
@@ -9,15 +10,17 @@ if (process.env.ENVIRONMENT === 'production') {
   port = process.env.PORT;
   host = process.env.HOST;
   serverUrl = `${host}`;
+  authenticate = true;
 } else if (process.env.ENVIRONMENT === 'development') {
   port = 3000;
   host = 'http://localhost';
   serverUrl = `${host}:${port}`;
+  authenticate = false;
 }
 
 export default {
   port,
   host,
   serverUrl,
-  authenticate: (process.env.ENVIRONMENT === 'development' && true) || true
+  authenticate
 };
