072650b475b15213ba7014857a0c47f9a32155e4
Chris Tram <chris.tram@gmail.com>
2018-06-01 18:36:29 -0700

    adds config variable for quick log in, to supersede authenticate flag.

--- app-config.js
+++ app-config.js
@@ -3,20 +3,25 @@ const environment = process.env.ENVIRONMENT || 'development';
 let port;
 let host;
 let serverUrl;
+let env;
 
 if (environment === 'production') {
   port = process.env.PORT;
   host = process.env.HOST;
   serverUrl = `${host}`;
+  env = 'production';
 } else if (environment === 'development') {
   port = 3000;
   host = 'http://localhost';
   serverUrl = `${host}:${port}`;
+  env = 'development';
 }
 
 export default {
   port,
   host,
   serverUrl,
-  authenticate: false
+  authenticate: true,
+  quickLogIn: true,
+  env
 };
