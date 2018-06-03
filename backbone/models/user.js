6ab0a79b95be6007023b970da2b8c026c61a685b
Chris Tram <chris.tram@gmail.com>
2018-06-02 19:20:42 -0700

    add logger

--- backbone/models/user.js
+++ backbone/models/user.js
@@ -2,6 +2,7 @@ import Backbone from 'backbone';
 
 export default class User extends Backbone.Model {
   urlRoot() {
+    console.log('app config on client', window.LEB.appConfig);
     return `${window.LEB.appConfig.serverUrl}/api/users`;
   }
 
