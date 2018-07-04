f6777b0bd90bc25fbbc39251435d601637c896b9
Chris Tram <chris.tram@gmail.com>
2018-07-04 12:18:10 -0700

    update image url for about page

--- assets/img-sources.js
+++ assets/img-sources.js
@@ -1,3 +1,3 @@
 export default {
-  previewWebsite: 'http://res.cloudinary.com/dt6ccpxqd/image/upload/c_scale,q_100,w_1200/v1528073491/Screen_Shot_2018-06-03_at_17.50.44_tqknif.jpg'
+  previewWebsite: 'http://res.cloudinary.com/dt6ccpxqd/image/upload/v1530242733/portfolio-site/live-events-bulletin.png'
 };
